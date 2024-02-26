624b958319c9e330a94b13199cb61f31d9e0755c
Army-py <61599555+Army-py@users.noreply.github.com>
2024-02-26 18:35:33 +0100

    feature: implement join / leave listeners

--- SingularityBukkit/src/main/java/fr/army/singularity/listener/ListenerLoader.java
+++ SingularityBukkit/src/main/java/fr/army/singularity/listener/ListenerLoader.java
@@ -1,12 +1,13 @@
 package fr.army.singularity.listener;
 
 import fr.army.singularity.SingularityPlugin;
+import fr.army.singularity.listener.impl.SessionListener;
 import org.bukkit.plugin.PluginManager;
 
 public class ListenerLoader {
 
     public void registerListeners(SingularityPlugin plugin) {
         final PluginManager pluginManager = plugin.getServer().getPluginManager();
-
+        pluginManager.registerEvents(new SessionListener(), plugin);
     }
 }
