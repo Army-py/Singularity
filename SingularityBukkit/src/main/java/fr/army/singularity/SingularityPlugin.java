090e6b621c129ee6f2713c9fabfec31ff47c395a
Army-py <61599555+Army-py@users.noreply.github.com>
2024-02-27 01:50:21 +0100

    feature: load config file

--- SingularityBukkit/src/main/java/fr/army/singularity/SingularityPlugin.java
+++ SingularityBukkit/src/main/java/fr/army/singularity/SingularityPlugin.java
@@ -1,14 +1,20 @@
 package fr.army.singularity;
 
+import fr.army.singularity.config.Config;
+import fr.army.singularity.config.ConfigLoader;
 import fr.army.singularity.listener.ListenerLoader;
 import fr.army.singularity.network.channel.ChannelRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.FileNotFoundException;
 
 public class SingularityPlugin extends JavaPlugin {
 
     public static SingularityPlugin plugin;
 
     private ChannelRegistry channelRegistry;
+    private ConfigLoader configLoader;
+    private Config config;
     private ListenerLoader listenerLoader;
 
     public void onEnable() {
@@ -16,6 +22,16 @@ public class SingularityPlugin extends JavaPlugin {
 
         channelRegistry = new ChannelRegistry();
         channelRegistry.register(this);
+
+        configLoader = new ConfigLoader(this);
+
+        try {
+            this.config = new Config(configLoader.initFile("config.yml"));
+        } catch (FileNotFoundException e) {
+            getLogger().severe("Unable to load config.yml");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
         listenerLoader = new ListenerLoader();
         listenerLoader.registerListeners(this);
@@ -39,5 +55,13 @@ public class SingularityPlugin extends JavaPlugin {
 
     public ListenerLoader getListenerLoader() {
         return listenerLoader;
+    }
+
+    public Config getConfiguration() {
+        return config;
+    }
+
+    public ConfigLoader getConfigLoader() {
+        return configLoader;
     }
 }
---
+++ SingularityBukkit/src/main/java/fr/army/singularity/config/ConfigLoader.java
@@ -0,0 +1,33 @@
+package fr.army.singularity.config;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Objects;
+
+public class ConfigLoader {
+
+    private final Plugin plugin;
+
+    public ConfigLoader(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public YamlConfiguration initFile(@NotNull String fileName) throws FileNotFoundException {
+        final File file = new File(plugin.getDataFolder(), fileName);
+        if (!file.exists()) {
+            try {
+                Files.copy(Objects.requireNonNull(plugin.getResource(fileName)), file.toPath());
+            } catch (IOException ignored) {
+                throw new FileNotFoundException("Unable to copy file from resources");
+            }
+        }
+        return YamlConfiguration.loadConfiguration(file);
+    }
+
+}
