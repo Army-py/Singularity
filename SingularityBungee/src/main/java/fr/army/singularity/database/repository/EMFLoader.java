0ab597164b8de3672f768fdb397f838e793b6c95
Army-py <61599555+Army-py@users.noreply.github.com>
2024-02-28 00:05:35 +0100

    fix: add persistence.xml file for ORM

--- SingularityBungee/src/main/java/fr/army/singularity/database/repository/EMFLoader.java
+++ SingularityBungee/src/main/java/fr/army/singularity/database/repository/EMFLoader.java
@@ -15,30 +15,41 @@ public class EMFLoader {
 
     private static EntityManagerFactory entityManagerFactory = null;
 
-    private static final boolean remoteDatabase = Config.storageMode == StorageMode.MYSQL;
-    private static final String databaseHost = Config.mysqlHost;
-    private static final int databasePort = Config.mysqlPort;
-    private static final String databaseName = Config.mysqlDatabase;
-    private static final String databaseUsername = Config.mysqlUsername;
-    private static final String databasePassword = Config.mysqlPassword;
+    private final boolean remoteDatabase;
+    private final String databaseHost;
+    private final int databasePort;
+    private final String databaseName;
+    private final String databaseUsername;
+    private final String databasePassword;
+    private final String sqliteFile;
 
-    private static final String sqliteFile = Config.sqliteFile;
+
+    public EMFLoader(){
+        remoteDatabase = Config.storageMode == StorageMode.MYSQL;
+        databaseHost = Config.mysqlHost;
+        databasePort = Config.mysqlPort;
+        databaseName = Config.mysqlDatabase;
+        databaseUsername = Config.mysqlUsername;
+        databasePassword = Config.mysqlPassword;
+        sqliteFile = Config.sqliteFile;
+    }
 
     public void setupEntityManagerFactory() {
         if (entityManagerFactory == null || !entityManagerFactory.isOpen()) {
             Properties properties = new Properties();
 
             if (remoteDatabase) {
-                properties.put("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
-                properties.put("javax.persistence.jdbc.url", "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + databaseName + "?autoReconnect=true");
-                properties.put("javax.persistence.jdbc.user", databaseUsername);
-                properties.put("javax.persistence.jdbc.password", databasePassword);
-                properties.put("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+                properties.put("jakarta.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
+                properties.put("jakarta.persistence.jdbc.url", "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + databaseName + "?autoReconnect=true");
+                properties.put("jakarta.persistence.jdbc.user", databaseUsername);
+                properties.put("jakarta.persistence.jdbc.password", databasePassword);
+                properties.put("hibernate.dialect", "org.hibernate.community.dialect.MariaDBDialect");
             } else {
-                properties.put("javax.persistence.jdbc.driver", "org.sqlite.JDBC");
-                properties.put("javax.persistence.jdbc.url", "jdbc:sqlite:./" + SingularityBungee.getPlugin().getDataFolder().getPath() + "/" + sqliteFile + "?autoReconnect=true");
-                properties.put("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect");
+                properties.put("jakarta.persistence.jdbc.driver", "org.sqlite.JDBC");
+                properties.put("jakarta.persistence.jdbc.url", "jdbc:sqlite:" + SingularityBungee.getPlugin().getDataFolder().getPath() + "/" + sqliteFile);
+                properties.put("hibernate.dialect", "org.hibernate.community.dialect.SQLiteDialect");
             }
+
 
             Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
             entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit", properties);
