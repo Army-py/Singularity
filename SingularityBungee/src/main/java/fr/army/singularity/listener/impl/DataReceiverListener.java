a93f0b8eb45f786a22d414a07f70695425a02e36
Army-py <61599555+Army-py@users.noreply.github.com>
2024-02-29 01:47:36 +0100

    fix: check channel name to avoid deserialization errors

--- SingularityBungee/src/main/java/fr/army/singularity/listener/impl/DataReceiverListener.java
+++ SingularityBungee/src/main/java/fr/army/singularity/listener/impl/DataReceiverListener.java
@@ -5,6 +5,7 @@ import fr.army.singularity.database.StorageManager;
 import fr.army.singularity.entity.impl.ConnectionLoggerEntity;
 import fr.army.singularity.entity.impl.PlayerHostLoggerEntity;
 import fr.army.singularity.entity.impl.PlayerLoggerEntity;
+import fr.army.singularity.network.channel.ChannelRegistry;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -25,20 +26,31 @@ public class DataReceiverListener implements Listener {
     @SuppressWarnings("unused")
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDataReceived(PluginMessageEvent event) {
+
+        if (!ChannelRegistry.CHANNEL.equals(event.getTag())){
+            System.out.println("Channel is not good");
+            System.out.println(event.getTag());
+            System.out.println(ChannelRegistry.CHANNEL);
+            return;
+        }
+
         final ByteArrayInputStream input = new ByteArrayInputStream(event.getData());
         try {
             final ObjectInputStream inputStream = new ObjectInputStream(input);
 
             final Object object = inputStream.readObject();
-            if (object instanceof PlayerLoggerEntity playerEntity) {
-                storageManager.savePlayerLogger(playerEntity);
-            } else if (object instanceof PlayerHostLoggerEntity hostEntity) {
-                storageManager.savePlayerHostLogger(hostEntity);
-            } else if (object instanceof ConnectionLoggerEntity connectionEntity) {
-                storageManager.saveConnectionLogger(connectionEntity);
+            if (object instanceof PlayerLoggerEntity.PlayerLoggerSnapshot playerSnapshot) {
+                storageManager.savePlayerLogger(PlayerLoggerEntity.fromSnapshot(playerSnapshot));
+            } else if (object instanceof PlayerHostLoggerEntity.PlayerHostLoggerSnapshot hostSnapshot) {
+                storageManager.savePlayerHostLogger(PlayerHostLoggerEntity.fromSnapshot(hostSnapshot));
+            } else if (object instanceof ConnectionLoggerEntity.ConnectionLoggerSnapshot connectionSnapshot) {
+                storageManager.saveConnectionLogger(ConnectionLoggerEntity.fromSnapshot(connectionSnapshot));
             }
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+
+        // storageManager.savePlayerLogger(PlayerLoggerEntity.readFromByte(event.getData()));
+        // System.out.println("PlayerLoggerEntity saved");
     }
 }
